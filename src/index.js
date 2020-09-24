d64d007eaa62e95f928cc363b6b4aaaee2fca708
Arafat Arman <arafatarman616@gmail.com>
2020-09-24 19:42:42 -0400

    laid out skeleton homepage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Home from './templates/Home'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/templates/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <div className="header">
+                <h1 className="header-title">Tweet Watcher</h1>
+                <h4 className="header-subtitle">Project by Arafat Arman</h4>
+            </div>
+
+            <div className="main-content">
+                <div className="user-form">
+                    <button className="btn">Blah</button>
+                </div>
+
+                <div className="vert-div"></div>
+
+                <div className="results">
+                    <button>Blah</button>
+                </div>
+            </div>  
+        </div>
+    );
+}
+export default Home;
