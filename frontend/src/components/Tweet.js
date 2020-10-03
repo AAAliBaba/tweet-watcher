050c98cb88d4bc0b456d70c60a71f64d8f9f657b
Arafat Arman <arafatarman616@gmail.com>
2020-10-02 21:58:28 -0400

    began tweet styling

--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import TweetModel from '../models/TweetModel'
+import './Tweet.css'
 
 function Tweet(props) {
     //pre: props is a tweet model
     // console.log(props)
     return (
-        <div>
-            {/* <p>{props.tweet}</p> */}
-            <img src={props.tweet.imgUrl}/>
-            <p>{props.tweet.username}</p>
-            <p>{props.tweet.usertag}</p>
-            <p>{props.tweet.message}</p>
-            <p>{props.tweet.datetime}</p>
+        <div className="tweet-root">
+            <div>
+                <img src={props.tweet.imgUrl}/>
+            </div>
+            <div>
+                <div className="tweet-userinfo">
+                    <p className="tweet-username">{props.tweet.username}</p>
+                    <p className="tweet-usertag">@{props.tweet.usertag}</p>
+                </div>
+                <p className="tweet-message">{props.tweet.message}</p>
+                <p className="tweet-datetime">{props.tweet.datetime}</p>
+            </div>
         </div>
     )
 }
--- frontend/src/templates/Home.js
+++ frontend/src/templates/Home.js
@@ -6,6 +6,14 @@ import Tweet from '../components/Tweet'
 import { render } from 'react-dom'
 
 function Home(props) {
+    const t = new TweetModel(
+        "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+        "AAAliBaba",
+        "aaalibaba616",
+        "this is a test tweet message",
+        "Sat Oct 03 00:04:31 +0000 2020"
+    )
+
     return (
         <div>
             <div className="header">
@@ -21,11 +29,14 @@ function Home(props) {
 
                 <div className="vert-div"></div>
 
-                <div id="div-results" className="results">
-                    <h2>Results</h2>
-                    {props.tweetList.map((t) => {
+                <div className="results">
+                    <div className="list">
+                        <h2 className="title">Results</h2>
+                        <div className="tweet"><Tweet tweet={t}/></div>
+                    </div>
+                    {/* {props.tweetList.map((t) => {
                         return <Tweet tweet={t}></Tweet>
-                    })}
+                    })} */}
                 </div>
             </div>  
         </div>
